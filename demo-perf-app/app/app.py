c4443e21ce572528268197879a9a129e61d7fe34
Pravin Kamble <iampbkamble@gmail.com>
2025-11-10 20:23:23 +0530

    Add slower logic to create regression

--- demo-perf-app/app/app.py
+++ demo-perf-app/app/app.py
@@ -1,8 +1,8 @@
 def sum_numbers(n=5_000_000):
     total = 0
     for i in range(n):
-        # total = total + i 
-        # for _ in range(5):
-        #     total += 1
+        total = total + i 
+        for _ in range(5):
+            total += 1
         total += i
     return total
