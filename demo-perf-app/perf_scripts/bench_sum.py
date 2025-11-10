a4b8f8ec306e404458f69d1c7ed3b29d6bad5f1e
Pravin <91125540+p-r-a-v-i-n@users.noreply.github.com>
2025-11-10 20:10:25 +0530

    Perf examples patch 3 (#6)
    * added good logic
    
    * Remove latest.json from repo
    
    * updated baseline and perf.yml
    
    * add perftrack repo main tag
    
    * add master branch for perftrack
    
    * added debugging in CI
    
    * reset baseline after packagin app
    
    * Fix import path and reset baseline

--- demo-perf-app/perf_scripts/bench_sum.py
+++ demo-perf-app/perf_scripts/bench_sum.py
@@ -1,8 +1,14 @@
-from app import sum_numbers
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) 
+
 import time
+from app.app import sum_numbers
 
 def run():
     start = time.perf_counter()
     sum_numbers()
     duration = time.perf_counter() - start
     print(f"duration_seconds={duration}")
+
+if __name__ == '__main__':
+    run()
